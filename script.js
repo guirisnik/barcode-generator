f400c92fcac8b8f4380dfd9dcc667de1e1f3697c
Guilherme Risnik <guilherme.risnik@gmail.com>
2023-12-06 22:16:36 -0300

    Add boleto bancario support

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.getElementById("btn").addEventListener("click", () => {
   let text = document.getElementById("text").value;
-  JsBarcode("#barcode", text);
+  const encoded = text.replace(
+    /^(\d{4})(\d{5})\d{1}(\d{10})\d{1}(\d{10})\d{1}(\d{15})$/,
+    "$1$5$2$3$4"
+  );
+  JsBarcode("#barcode", encoded, { format: "ITF" });
 });
